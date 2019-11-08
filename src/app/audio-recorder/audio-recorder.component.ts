b367f468c07d4b71d2961cf47833efed092be56a
Al Gae <algae1234567890@gmail.com>
2019-11-08 21:58:18 +0800

    up

--- src/app/audio-recorder/audio-recorder.component.ts
+++ src/app/audio-recorder/audio-recorder.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { interval, timer, Observable, Subscription, ObservableLike }  from 'rxjs';
-import { map, take, finalize } from 'rxjs/operators';
+import { interval, timer, Observable, Subscription, ObservableLike, of, from, iif, empty, fromEvent, pipe, Subject }  from 'rxjs';
+import { map, take, finalize, takeWhile, mergeMap, publish, tap, scan } from 'rxjs/operators';
 
 declare const MediaRecorder: any;
 
@@ -25,8 +25,13 @@ export class AudioRecorderComponent implements OnInit {
   runtime: number;
   timerSub: Subscription;
   completed = false;
-  aRuntime: Observable<number>;
+  aRuntime;
+  aRecorderState: Observable<string>;
   src = null;
+  aSrc: Observable<string>;
+  shouldStop = false;
+  subState;
+  timeSubj;
 
   constructor() {
     navigator.permissions.query({name:'microphone'}).then((result) => {
@@ -65,7 +70,26 @@ export class AudioRecorderComponent implements OnInit {
     }
     this.requestStatus = 'granted';
     this.recorderState = 'inactive';
-    console.log(this.player);
+    this.aRecorderState = interval(1).pipe(map(v => this.recorderState));
+    this.aSrc = interval(1).pipe(map(v => this.src));
+    this.aRuntime = interval(1000)
+      .pipe(
+        tap(l => { this.runtime = l }),
+        takeWhile(v => this.recorderState !== 'paused' && !this.shouldStop)
+      )
+      .pipe(take(this.recordLimit))
+      .pipe(publish());
+      /*.pipe(map(x => x))
+      //.pipe(iif(() => this.recorderState === 'recording', of(0), of(0)))
+      .pipe(takeWhile(v => this.recorderState === 'recording' && !this.shouldStop))
+      .pipe(take(this.recordLimit))
+      .pipe(
+        finalize(() => {
+          console.log('Recording finished');
+          // if (this.shouldStop) this.stopRecording();
+        })
+      )
+      .pipe(publish())*/
   };
 
   requestPermission() {
@@ -88,18 +112,25 @@ export class AudioRecorderComponent implements OnInit {
     console.log('started');
     this.recorderState = 'recording';
     this.timer = interval(1000);
-    this.aRuntime = interval(1000)
-      .pipe(map(x => x + 1))
-      .pipe(
-        take(this.recordLimit),
-        finalize(() => {
-          console.log('Recording finished');
-          this.stopRecording();
-        }
-      ));
+    const conn = this.aRuntime.connect();
+    // this.aRuntime = interval(1000)
+    //   .pipe(map(x => x))
+    //   //.pipe(iif(() => this.recorderState === 'recording', of(0), of(0)))
+    //   .pipe(takeWhile(v => this.recorderState === 'recording' && !this.shouldStop))
+    //   .pipe(take(this.recordLimit))
+    //   .pipe(
+    //     finalize(() => {
+    //       console.log('Recording finished');
+    //       // if (this.shouldStop) this.stopRecording();
+    //     })
+    //   )
+    //   .pipe(
+    //     publish()
+    //   )
   }
 
   stopRecording() {
+    this.shouldStop = true;
     this._mediaRecorder.stop();
   }
   stopHandler() {
